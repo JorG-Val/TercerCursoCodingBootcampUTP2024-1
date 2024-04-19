1c682f12ee03fe5626816bf488ac80cbb68d880a
JorG_Val <47404504+JorG-Val@users.noreply.github.com>
2024-04-19 12:09:12 -0500

    v-21

--- JavaScriptProyecto2/1_SerieFibonacci.js
+++ JavaScriptProyecto2/1_SerieFibonacci.js
@@ -1,7 +1,7 @@
 /* -Fecha de publicación: 19-04-2024
--Hora de publicación: 9:10 AM
--Versión de su código: 20
--Autores:Ing(c): Jorge Andres Valderrama Baron
+-Hora de publicación: 12:08 PM
+-Versión de su código: 21
+-Autores:Ing(c): Jorge Andres Valderrama - Mariand Evans - Juan Manuel Vargas Rios - Kevin leiton - Manuela Cardona - Santigo Melo
 -Nombre del lenguaje : ECMASCRIPT
 -Versión del lenguaje utilizado: 6.0
 -Universidad Tecnológica de Pereira
